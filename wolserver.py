d8da02e90267cf1a89a2dabf01eb4b4b1bf149c4
Timothy Do <timothydobsa@gmail.com>
2023-09-16 23:42:24 -0700

    Added Input for Port and Execute It :)

--- wolserver.py
+++ wolserver.py
@@ -55,4 +55,4 @@ def upload():
 # Debug if the same file as run
 if __name__ == "__main__":
 	port = int(input('Choose a Port to Host the WOL Server: '))
-	app.run(debug=True,host='0.0.0.0',port=8080)
+	app.run(debug=True,host='0.0.0.0',port=port)
